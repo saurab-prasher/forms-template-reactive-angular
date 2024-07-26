cd23ad61f3ef00bfc97a3b30719942c9a167108c
saurabprasher <saurab015@gmail.com>
2024-07-25 20:07:56 -0400

    reactive forms completed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -7,7 +7,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -26,6 +26,14 @@ function emailIsUnique(control: AbstractControl) {
   return of({ notUnique: true });
 }
 
+let initialEmailValue = '';
+
+const savedForm = window.localStorage.getItem('saved-login-form');
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -33,7 +41,8 @@ function emailIsUnique(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   // private form = viewChild.required<NgForm>('form');
   // private destroyRef = inject(DestroyRef);
   // constructor() {
@@ -69,8 +78,29 @@ export class LoginComponent {
   //   }
   // }
 
+  ngOnInit() {
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+    //   });
+    // }
+
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) =>
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          ),
+      });
+
+    this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+  }
+
   form = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.email],
       asyncValidators: [emailIsUnique],
     }),
